778e424575d00aa48be8def9ad35ef5ba112502a
AndrikV <andriutsyo@gmail.com>
2022-08-22 23:36:27 +0300

    apps/polls: models, basic structure. No interesting features

--- my_blog/polls/models.py
+++ my_blog/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Polls(models.Model):
+    question = models.CharField(max_length=256)
+    description = models.TextField(null=True)
+    allow_multi_choices = models.BooleanField(default=False)
+    datetime_of_publication = models.DateTimeField(auto_now_add=True)
+
+
+class Choices(models.Model):
+    poll = models.ForeignKey(Polls)
+    name = models.CharField(max_length=256)
+    author_info = models.CharField(max_length=256, null=True)
+    
